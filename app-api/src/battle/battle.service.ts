bfca6e4e049e5085b3035d0e3b97112d92f6276d
RomanKrohin <romankrohin126@gmail.com>
2025-01-14 16:29:43 +0300

    fix: battle keys

--- app-api/src/battle/battle.service.ts
+++ app-api/src/battle/battle.service.ts
@@ -8,7 +8,7 @@ export class BattleService {
 
   async createBattle(battleId: string, userId: string) {
     const battle = { userId, status: 'waiting' };
-    await this.redisService.updateSession(battleId, JSON.stringify(battle));
+    await this.redisService.updateSession("Battle "+battleId, JSON.stringify(battle));
   }
 
   async checkBattle(battleId: string): Promise<boolean> {
@@ -19,16 +19,15 @@ export class BattleService {
 
   async getAllBattles(id: string): Promise<string> {
     try {
-      const keys = await this.redisService.getKeys();
-      console.log(`Количество ключей: ${keys.length}`);
+      const keys = (await this.redisService.getKeys()).filter(it => it.includes("Battle "));
       if (keys.length==0){
         const key_tmp = randomUUID()
         this.createBattle(key_tmp,id)
-        return key_tmp
+        return key_tmp.replace("Battle ", "")
       }
       const current_key = keys[0];
       this.redisService.cleanSession(current_key)
-      return current_key;
+      return current_key.replace("Battle ", "");
     } catch (error) {
       console.error('Ошибка при получении ключей:', error);
       throw error;
